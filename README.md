9001e4fe2c2ebb1c257b044d79b2e9f08dd9772b
BEOMKING <qjawlsqjacks@naver.com>
2022-07-31 22:55:41 +0900

    Doller의 곱셈 기능 TDD

---
+++ README.md
@@ -0,0 +1,2 @@
+# Test-Driven-Development
+켄트백의 테스트 주도 개발 예제
---
+++ src/test/java/com/practice/tdd/domain/DollarTest.java
@@ -0,0 +1,17 @@
+package com.practice.tdd.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DollarTest {
+
+    @Test
+    void testMultiplication() {
+        Dollar five = new Dollar(5);
+
+        five.times(2);
+
+        assertEquals(10, five.amount);
+    }
+}
