6f4d8505fad2d9a071dbc612f41737e785f08d8d
BEOMKING <qjawlsqjacks@naver.com>
2022-08-06 10:51:51 +0900

    달러 프랑 제거

--- src/test/java/com/practice/tdd/domain/DollarTest.java
+++ src/test/java/com/practice/tdd/domain/DollarTest.java
@@ -3,9 +3,7 @@ package com.practice.tdd.domain;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DollarTest {
 
@@ -21,8 +19,6 @@ public class DollarTest {
     void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(6));
-        assertEquals(Money.franc(5), Money.franc(5));
-        assertNotEquals(Money.franc(5), Money.franc(6));
         assertNotEquals(Money.franc(5), Money.dollar(5));
     }
 }
